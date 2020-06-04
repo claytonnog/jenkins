47cc33940aa95550e1893ca371f7440de4991166
Clayton Neves <clayton.nog@gmail.com>
2020-06-04 17:30:16 -0300

    alterado job ok

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/claytonnog/jenkins.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('Clayton Neves')
-            node / gitConfigEmail('clayton.nog@gmail.com')
+        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
     triggers {
